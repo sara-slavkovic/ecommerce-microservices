11ab0d3abb3c78b1d05c1f565888bc32c64d30b0
sara-slavkovic <saraslavkovic3@gmail.com>
2026-03-27 23:27:21 +0100

    CategoriesController

---
+++ CatalogService.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,71 @@
+﻿using CatalogService.Application.DTOs;
+using CatalogService.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogService.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var categories = await _categoryService.GetAllCategoriesAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetCategoryById(Guid id)
+        {
+            var category = await _categoryService.GetCategoryByIdAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryDto dto)
+        {
+            var createdCategory = await _categoryService.CreateCategoryAsync(dto);
+            return CreatedAtAction(nameof(GetCategoryById), new { id = createdCategory.Id }, createdCategory);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateCategory(Guid id, [FromBody] UpdateCategoryDto dto)
+        {
+            var updatedCategory = await _categoryService.UpdateCategoryAsync(id, dto);
+
+            if (updatedCategory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(updatedCategory);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteCategory(Guid id)
+        {
+            var deleted = await _categoryService.DeleteCategoryAsync(id);
+
+            if (!deleted)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
--- CatalogService.Api/Program.cs
+++ CatalogService.Api/Program.cs
@@ -10,6 +10,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<CatalogService.Infrastructure.Persistence.CatalogDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CatalogDatabase")));
 
+builder.Services.AddScoped<CatalogService.Application.Interfaces.ICategoryRepository, CatalogService.Infrastructure.Repositories.CategoryRepository>();
+builder.Services.AddScoped<CatalogService.Application.Interfaces.ICategoryService, CatalogService.Application.Services.CategoryService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
