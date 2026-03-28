b9c88e5226f075b8c57f274913752b8cfe818f47
sara-slavkovic <saraslavkovic3@gmail.com>
2026-03-28 14:24:30 +0100

    Product DTOs

--- CatalogService.Application/DTOs/CategoryDto.cs
+++ CatalogService.Application/DTOs/CategoryDto.cs
@@ -7,7 +7,7 @@ namespace CatalogService.Application.DTOs
     public class CategoryDto
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public Guid? ParentCategoryId { get; set; } 
     }
 }
---
+++ CatalogService.Application/DTOs/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatalogService.Application.DTOs
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Sku { get; set; } = string.Empty; 
+        public string Name { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
