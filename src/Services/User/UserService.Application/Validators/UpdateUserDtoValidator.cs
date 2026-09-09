e1ec45e83ceb9e8e76d511bf7a026665892c44a3
sara-slavkovic <saraslavkovic3@gmail.com>
2026-09-09 12:07:23 +0200

    Namespace fix in UpdateUserDtoValidator

--- src/Services/User/UserService.Application/Validators/UpdateUserDtoValidator.cs
+++ src/Services/User/UserService.Application/Validators/UpdateUserDtoValidator.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace CartService.Application.Validators
+namespace UserService.Application.Validators
 {
     public class UpdateUserDtoValidator : AbstractValidator<UpdateUserDto>
     {
